0ff4bf914b1416595ba7a57b0b45cbea6e2735e7
fatima678 <119948111+fatima678@users.noreply.github.com>
2024-02-16 16:21:09 +0500

    Set the route for pages using react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import {BrowserRouter, Routes,Route} from " react-router-dom";
+import ShopCategory from './pages/ShopCategory';
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
       <Navbar/>
+      <Routes>
+        <Route path="/" element={<shop/>}></Route>
+        <Route path="/men" element={<ShopCategory category="men"/>}></Route>
+        <Route path="/women" element={<ShopCategory category="women"/>}></Route>
+        <Route path="/kids" element={<ShopCategory category="kids"/>}></Route>
+      </Routes>
+      </BrowserRouter>
+      
       
     </div>
   );
